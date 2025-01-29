7dfa7d9f4592b66390c16d9bd12ce89052377e56
Tigran Gevorgyan <tigran.gevorgyan@cognaize.com>
2025-01-29 15:30:19 +0400

    minor changes

--- src/features/user/CreateUser.jsx
+++ src/features/user/CreateUser.jsx
@@ -25,7 +25,7 @@ function CreateUser() {
 
       <input
         type="text"
-        placeholder="Your full name"
+        placeholder="Your full Name"
         value={username}
         onChange={(e) => setUsername(e.target.value)}
         className="input mb-8 w-72"
